3e6be64cea1572fb781848d57d3ae8f5207b263d
tudorfis <tudor.fis@gmail.com>
2022-06-14 13:20:21 +0300

    changes

---
+++ src/components/InnerOutterWrapped.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <div 
+    ref="innerContent"
+    style="border: 3px solid blue; padding: 3rem;"
+  >
+    <h1>inner content</h1>
+  </div>
+  <div 
+    ref="wrapperContent" 
+    v-show="isWrapped"
+    style="border: 3px solid red; padding: 3rem;"
+  />
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+  name: 'InnerOutterWrapped',
+  props: {
+    isWrapped: {
+      type: Boolean,
+      default: true
+    }
+  },
+  setup(props) {
+    const innerContent = ref()
+    const wrapperContent = ref()
+
+    onMounted(() => {
+      if (props.isWrapped) {
+        wrapperContent.value.append(innerContent.value)
+      }
+    })
+
+    return {
+      innerContent,
+      wrapperContent,
+    }
+  },
+}
+</script>
